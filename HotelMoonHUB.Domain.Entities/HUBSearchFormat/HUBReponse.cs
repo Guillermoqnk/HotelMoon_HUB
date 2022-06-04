1b7b7e6b625aa95fedcee30ea8df5622562d9010
Wyllink <guillermoqnk@gmail.com>
2022-06-04 11:41:05 +0200

    Added Basic Entities closes #2

---
+++ HotelMoonHUB.Domain.Entities/HUBSearchFormat/HUBReponse.cs
@@ -0,0 +1,19 @@
+﻿
+public class HUBReponse
+{
+    public Room[] rooms { get; set; }
+}
+
+public class Room
+{
+    public int roomId { get; set; }
+    public Rate[] rates { get; set; }
+}
+
+public class Rate
+{
+    public int mealPlanId { get; set; }
+    public bool isCancellable { get; set; }
+    public float price { get; set; }
+}
+
---
+++ HotelMoonHUB.Domain.Entities/HotelLegsSearchFormat/HotelLegsRequest.cs
@@ -0,0 +1,10 @@
+﻿
+public class HotelLegsRequest
+{
+    public int hotel { get; set; }
+    public string checkInDate { get; set; }
+    public int numberOfNights { get; set; }
+    public int guests { get; set; }
+    public int rooms { get; set; }
+    public string currency { get; set; }
+}
