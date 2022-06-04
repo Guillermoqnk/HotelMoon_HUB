3785c8b117437dd7699127203183d25603e8c17c
Wyllink <guillermoqnk@gmail.com>
2022-06-04 12:22:55 +0200

    Basic structure for service

---
+++ HotelMoonHUB.Application.Services/Service.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMoonHUB.Application.Services
+{
+    public class Service
+    {
+        public void Search()
+        {
+            //Llamada a la lista
+            //Unificación de respuestas
+        }
+    }
+}
