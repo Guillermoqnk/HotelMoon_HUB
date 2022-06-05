9f6c65332612c2400562f05c33003c447312f565
Wyllink <guillermoqnk@gmail.com>
2022-06-06 01:54:15 +0200

    Added Versions Fucntions closes #12

--- HotelMoonHUB.WebAPI.Services/Program.cs
+++ HotelMoonHUB.WebAPI.Services/Program.cs
@@ -1,6 +1,8 @@
 using HotelMoonHUB.Application.Services;
 using HotelMoonHUB.Application.Services.Contracts;
 using HotelMoonHUB.Application.Services.Implementations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(x =>
+    {
+        x.DefaultApiVersion = new ApiVersion(1, 0);
+        x.AssumeDefaultVersionWhenUnspecified = true;
+        x.ReportApiVersions = true;
+        x.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+    });
 
 var app = builder.Build();
 
