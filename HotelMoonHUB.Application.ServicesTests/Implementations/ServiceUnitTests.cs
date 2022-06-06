3a465ed4a399d7866dc7ac2ed19cad04caa8a158
Wyllink <guillermoqnk@gmail.com>
2022-06-06 02:00:33 +0200

    Inecesary references cleaned up

--- HotelMoonHUB.Application.ServicesTests/Implementations/ServiceUnitTests.cs
+++ HotelMoonHUB.Application.ServicesTests/Implementations/ServiceUnitTests.cs
@@ -1,9 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using HotelMoonHUB.Application.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Moq;
 using HotelMoonHUB.Application.Services.Contracts;
--- HotelMoonHUB.Infrastructure.SvcAgents/Contracts/IHotelLegsConnection.cs
+++ HotelMoonHUB.Infrastructure.SvcAgents/Contracts/IHotelLegsConnection.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HotelMoonHUB.Infrastructure.SvcAgents.Contracts
+﻿namespace HotelMoonHUB.Infrastructure.SvcAgents.Contracts
 {
     public interface IHotelLegsConnection
     {
